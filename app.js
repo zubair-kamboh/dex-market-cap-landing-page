ff7130c9d8f4e39a12d05f680db482c8f43ec5a2
zubair ali <zubairkamboh9010@gmail.com>
2021-12-24 10:52:10 +0500

    Made site responsive

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
   $('#example').DataTable({
     select: true,
+    scrollX: true,
+    // searching: false,
   })
+
+  // $('#example').DataTable({
+  //   // headerLength: 6,
+
+  //   rowEditor: false,
+  // })
 })
